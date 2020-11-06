b61a1c894ed5c47dd7d68204fb13861c0104290d
tkim294 <tkim294@wisc.edu>
2020-11-05 16:14:36 -0800

    display messages in chat room

--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -1,13 +1,23 @@
 import React from "react";
-import { Text } from "react-native";
+import { FlatList, ImageBackground, Text } from "react-native";
 
 import { useRoute } from "@react-navigation/native";
+
+import chatRoomData from '../data/Chats';
+import ChatMessage from "../components/ChatMessage";
+import BG from '../assets/images/BG.png';
 
 const ChatRoomScreen = () => {
   const route = useRoute();
 
   return(
-    <Text>Chat Room</Text>
+    <ImageBackground style={{width: '100%', height: '100%'}} source={BG} >
+      <FlatList
+          data={chatRoomData.messages}
+          renderItem={({item}) => <ChatMessage message={item} /> }
+          inverted
+      />
+    </ImageBackground>
   );
 };
 
