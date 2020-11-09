816c952eaf7c2d9e22a9fd8782fcbde8b5504503
tkim294 <tkim294@wisc.edu>
2020-11-09 15:31:16 -0800

    implement api and user model

--- App.tsx
+++ App.tsx
@@ -1,20 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
 
-import { withAuthenticator } from 'aws-amplify-react-native'
+import { withAuthenticator } from "aws-amplify-react-native";
 
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
+import { getUser } from "./graphql/queries";
+import { createUser } from "./graphql/mutations";
+
+import Amplify, { Auth, API, graphqlOperation } from "aws-amplify";
+import config from "./aws-exports";
 Amplify.configure(config);
+
+const randomImages = [
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-2.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-3.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-6.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-9.jpg',
+];
 
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const getRandomImage = () => {
+    return randomImages[Math.floor(Math.random() * randomImages.length)];
+  };
+
+  // run this snippet only when App is first mounted
+  useEffect(() => {
+    const fetchUser = async () => {
+      // get Authenticated user from Auth
+      const userInfo = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+
+      if (userInfo) {
+        // get the user from Backend with the user Id from Auth
+        const userData = await API.graphql(
+          graphqlOperation(
+            getUser, 
+            { id: userInfo.attributes.sub }
+            )
+        );
+
+        if(userData.data.getUser) {
+          console.log("User is already registered in database");
+          return;
+        }
+
+        // if there is no user in DB with the id, then create one
+        const newUser = {
+          id: userInfo.attributes.sub,
+          name: userInfo.username,
+          imageUri: getRandomImage(),
+          status: 'Hey, I am using WhatsApp',
+        }
+
+        await API.graphql(
+          graphqlOperation(
+            createUser,
+            { input: newUser }
+          )
+        );
+
+      }
+    };
+
+    fetchUser();
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
@@ -28,4 +85,4 @@ function App() {
   }
 }
 
-export default withAuthenticator(App)
+export default withAuthenticator(App);
