c09d9f2ef10f6bebb5f98ab68a2434347c24af36
tkim294 <tkim294@wisc.edu>
2020-11-10 15:49:54 -0800

    fetch and display messages

--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -1,22 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList, ImageBackground, Text } from "react-native";
 
 import { useRoute } from "@react-navigation/native";
 
-import chatRoomData from '../data/Chats';
+import {API, Auth, graphqlOperation} from 'aws-amplify';
+
+import {messagesByChatRoom} from '../graphql/queries';
+
 import ChatMessage from "../components/ChatMessage";
 import BG from '../assets/images/BG.png';
 import InputBox from "../components/InputBox";
 
 const ChatRoomScreen = () => {
 
+  const [messages, setMessages] = useState([]);
+  const [myId, setMyId] = useState(null);
+
   const route = useRoute();
+
+
+  useEffect(() => {
+    const fetchMessages = async () => {
+      const messagesData = await API.graphql(
+        graphqlOperation(
+          messagesByChatRoom, {
+            chatRoomID: route.params.id,
+            sortDirection: "DESC"
+          }
+        )
+      )
+      setMessages(messagesData.data.messagesByChatRoom.items);
+    }
+    fetchMessages();
+  }, [])
+
+  useEffect(() => {
+    const getMyId = async () => {
+      const userInfo = await Auth.currentAuthenticatedUser();
+      setMyId(userInfo.attributes.sub);
+    }
+    getMyId();
+  }, [])
 
   return(
     <ImageBackground style={{width: '100%', height: '100%'}} source={BG} >
       <FlatList
-          data={chatRoomData.messages}
-          renderItem={({item}) => <ChatMessage message={item} /> }
+          data={messages}
+          renderItem={({item}) => <ChatMessage myId={myId} message={item} /> }
           inverted
       />
       <InputBox chatRoomID={route.params.id} />
